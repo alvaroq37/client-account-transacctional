e37e2b2b05fb6d60c98218bd80668b730bc7ac71
alvaroq37 <alvaro_q23@hotmail.com>
2025-01-04 14:39:19 -0400

    Se agrega la lógica del AccountImpl

--- src/main/java/account/impl/AccountImpl.java
+++ src/main/java/account/impl/AccountImpl.java
@@ -14,6 +14,8 @@ import jakarta.inject.Inject;
 import product.dao.data.Product;
 import product.dao.repository.ProductRepository;
 import util.Messages;
+
+import java.util.Date;
 import java.util.List;
 
 @ApplicationScoped
@@ -31,24 +33,24 @@ public class AccountImpl {
     @Inject
     ProductRepository productRepository;
 
-    public JsonObject accountListAll(){
-        try{
+    public JsonObject accountListAll() {
+        try {
             List<Account> accounts = accountRepository.accountListAll();
-            if(!accounts.isEmpty()){
+            if (!accounts.isEmpty()) {
                 return new JsonObject().put("response", accounts);
             }
             return messages.messageListEmpty();
-        }catch (Exception e){
+        } catch (Exception e) {
             return messages.messageError();
         }
     }
 
-    public JsonObject findAccountById(JsonObject data){
-        try{
+    public JsonObject findAccountById(JsonObject data) {
+        try {
             Long idAccount = data.getLong("idAccount");
-            if(idAccount > 0){
+            if (idAccount > 0) {
                 Account account = accountRepository.accountFindById(idAccount);
-                if(account.numberAccount > 0){
+                if (account.numberAccount > 0) {
                     return new JsonObject().put("response", account);
                 }
                 return messages.messageListEmpty();
@@ -59,27 +61,78 @@ public class AccountImpl {
         }
     }
 
-    public JsonObject createAccount(JsonObject data){
-        JsonObject clientData = data.getJsonObject("client");
-        JsonObject productData = data.getJsonObject("product");
-        JsonObject currencyData = data.getJsonObject("currency");
-        JsonObject branchData = data.getJsonObject("branch");
+    public JsonObject createAccount(JsonObject data) {
+        try {
+            JsonObject clientData = data.getJsonObject("client");
+            JsonObject productData = data.getJsonObject("product");
+            JsonObject currencyData = data.getJsonObject("currency");
+            JsonObject branchData = data.getJsonObject("branch");
 
-        Client client = clientRepository.clientFindById(clientData.getLong("clientId"));
-        Product product = productRepository.findProductById(productData.getLong("productId"));
-        Currency currency = currencyRepository.currencyFindById(currencyData.getLong("currencyId"));
-        Branch branch = branchRepository.agencyFindById(branchData.getLong("branchId"));
+            Account account = new Account();
 
-        if(client.idClient > 0){
-            if(product.idProduct > 0){
-                if(currency.idCurrency > 0){
-                    if(branch.id > 0){
-
-                    }
+            if (!clientData.isEmpty()) {
+                Client client = clientRepository.clientFindById(clientData.getLong("clientId"));
+                if (client.idClient > 0) {
+                    account.client = client;
+                } else {
+                    return messages.messageListEmpty();
                 }
+                if (!productData.isEmpty()) {
+                    Product product = productRepository.findProductById(productData.getLong("productId"));
+                    if (product.idProduct > 0) {
+                        account.product = product;
+                    } else {
+                        return messages.messageListEmpty();
+                    }
+                    if (!currencyData.isEmpty()) {
+                        Currency currency = currencyRepository.currencyFindById(currencyData.getLong("currencyId"));
+                        if (currency.idCurrency > 0) {
+                            account.currency = currency;
+                        } else {
+                            return messages.messageListEmpty();
+                        }
+                        if (!branchData.isEmpty()) {
+                            Branch branch = branchRepository.agencyFindById(branchData.getLong("branchId"));
+                            if (branch.id > 0) {
+                                account.branch = branch;
+                            } else {
+                                return messages.messageListEmpty();
+                            }
+                        } else {
+                            return messages.messageDataInput();
+                        }
+                    } else {
+                        return messages.messageDataInput();
+                    }
+                } else {
+                    return messages.messageDataInput();
+                }
+            } else {
+                return messages.messageDataInput();
             }
 
+            account.numberAccount = data.getLong("numberAccount");
+            account.amount = data.getLong("amount");
+            account.dateCreate = new Date();
+            account.userCreate = data.getInteger("userCreate");
+            accountRepository.accountSave(account);
+            return new JsonObject().put("response", "La cuenta ha sido registrada correctamente");
+        } catch (Exception e) {
+            return messages.messageError();
         }
-        return null;
+    }
+
+    public JsonObject deleteAccount(JsonObject data){
+        try{
+            Long accountId = data.getLong("accountId");
+            if(accountId > 0){
+                accountRepository.accountDelete(accountId);
+                return new JsonObject().put("response", "La cuenta ha sido eliminada");
+            }else{
+                return messages.messageDataInput();
+            }
+        }catch (Exception e){
+            return messages.messageError();
+        }
     }
 }
